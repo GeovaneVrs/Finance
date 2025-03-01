7271e1c2d4bea51ffb7b49f8af48da8b6ff9b0bd
RM-1807 <geovanevalerio1807@gmail.com>
2025-03-01 11:35:13 -0300

    Modicações na tela home

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 import BottomNavbar from '@/components/bar';
 
 export default function FinanceDashboard() {
@@ -10,21 +11,46 @@ export default function FinanceDashboard() {
         {/* Informações Financeiras */}
         <View style={styles.financeInfo}>
           <View style={styles.balanceContainer}>
+            {/* Total Balance */}
             <View style={styles.balanceSection}>
-              <Text style={styles.totalBalanceLabel}>Total Balance</Text>
+              <View style={styles.labelContainer}>
+                <View style={styles.icon}>
+                  <Svg width="8" height="8" viewBox="0 0 8 8" fill="none">
+                    <Path d="M7.5 1C7.5 0.723857 7.27614 0.5 7 0.5L2.5 0.5C2.22386 0.5 2 0.723858 2 1C2 1.27614 2.22386 1.5 2.5 1.5L6.5 1.5L6.5 5.5C6.5 5.77614 6.72386 6 7 6C7.27614 6 7.5 5.77614 7.5 5.5L7.5 1ZM1.35355 7.35355L7.35355 1.35355L6.64645 0.646447L0.646447 6.64645L1.35355 7.35355Z" fill="#052224"/>
+                  </Svg>
+                </View>
+                <Text style={styles.totalBalanceLabel}>Total Balance</Text>
+              </View>
               <Text style={styles.totalBalanceValue}>$7,783.00</Text>
             </View>
 
             <View style={styles.divider} />
 
+            {/* Total Expense */}
             <View style={styles.balanceSection}>
-              <Text style={styles.totalExpenseLabel}>Total Expense</Text>
+              <View style={styles.labelContainer}>
+                <View style={styles.icon}>
+                  <Svg width="8" height="8" viewBox="0 0 8 8" fill="none">
+                    <Path d="M7 7.5C7.27614 7.5 7.5 7.27614 7.5 7L7.5 2.5C7.5 2.22386 7.27614 2 7 2C6.72386 2 6.5 2.22386 6.5 2.5L6.5 6.5L2.5 6.5C2.22386 6.5 2 6.72386 2 7C2 7.27614 2.22386 7.5 2.5 7.5L7 7.5ZM0.646447 1.35355L6.64645 7.35355L7.35355 6.64645L1.35355 0.646447L0.646447 1.35355Z" fill="#052224"/>
+                  </Svg>
+                </View>
+                <Text style={styles.totalExpenseLabel}>Total Expense</Text>
+              </View>
               <Text style={styles.totalExpenseValue}>-$1,187.40</Text>
             </View>
           </View>
 
           {/* Retângulo abaixo das informações financeiras */}
-          <View style={styles.bottomBox} />
+          <View style={styles.bottomBoxContainer}>
+            {/* Parte preta de fundo */}
+            <View style={styles.blackBackground}>
+              <Text style={styles.percentageText}>30%</Text>
+            </View>
+            {/* Parte branca sobreposta */}
+            <View style={styles.whiteOverlay}>
+              <Text style={styles.amountText}>$20,000.00</Text>
+            </View>
+          </View>
         </View>
       </View>
 
@@ -64,17 +90,22 @@ const styles = StyleSheet.create({
   balanceContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10, // Adiciona um espaço antes do retângulo
+    marginBottom: 10, 
   },
   balanceSection: {
     alignItems: 'center',
     paddingHorizontal: 20,
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   totalBalanceLabel: {
     color: '#093030',
     fontSize: 12,
     fontFamily: 'Poppins-Regular',
     textTransform: 'capitalize',
+    marginLeft: 5,
   },
   totalBalanceValue: {
     color: '#F1FFF3',
@@ -84,8 +115,8 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   divider: {
-    width: 1, // Deixa a linha em pé
-    height: 40, // Ajuste para a altura desejada
+    width: 1, 
+    height: 40, 
     backgroundColor: '#DFF7E2',
     marginHorizontal: 15,
   },
@@ -94,6 +125,7 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: 'Poppins-Regular',
     textTransform: 'capitalize',
+    marginLeft: 5, 
   },
   totalExpenseValue: {
     color: '#0068FF',
@@ -101,12 +133,50 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-SemiBold',
     textTransform: 'capitalize',
   },
-  bottomBox: {
+  icon: {
+    width: 12,
+    height: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomBoxContainer: {
     width: 330,
+    height: 27,
+    marginTop: 10,
+    position: 'relative',
+  },
+  blackBackground: {
+    position: 'absolute',
+    width: '100%',
     height: 27,
     backgroundColor: '#052224',
     borderRadius: 13.5,
-    marginTop: 10, 
+    justifyContent: 'center',
+    paddingLeft: 15,
+  },
+  whiteOverlay: {
+    position: 'absolute',
+    width: '80%',
+    height: 27,
+    backgroundColor: '#f1fff3',
+    borderRadius: 13.5,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'flex-end', 
+    paddingRight: 15, 
+  },
+  percentageText: {
+    color: '#f1fff3',
+    fontSize: 13,
+    fontFamily: 'Poppins-Medium',
+    textTransform: 'capitalize',
+  },
+  amountText: {
+    color: '#052224',
+    fontSize: 13,
+    fontFamily: 'Poppins-Medium',
+    textTransform: 'capitalize',
+    fontStyle: 'italic', 
   },
   whiteContainer: {
     width: '100%',
@@ -127,4 +197,3 @@ const styles = StyleSheet.create({
     borderRadius: 31,
   },
 });
-
